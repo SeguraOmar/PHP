174ddaab556d339c1503990b8124051a63d2c8f3
Omar <omarsegura76300@gmail.com>
2024-01-08 14:41:53 +0100

    combat

--- index.php
+++ index.php
@@ -24,6 +24,18 @@ $Orc = new Orc('Guldar',2000,0,'Feu');
 
 <?php 
 $hero->getInfos();
+// echo $hero->getName() .  " Attaque " . $Orc->getName() . " de " .  $hero->getWeaponDamage() . " dégâts " . "<br>";
+//  echo "Il reste " .  $Orc->getHealth() - $hero->getWeaponDamage() . " Points de vie à " . $Orc->getName() . "<br>"; 
+ 
+
+//  $Orc->attack();
+// echo $Orc->getName() . " Attaque " . $hero->getName() . " de " . $Orc->getDamage() . " dégâts " . "<br>";
+// $hero->beAttacked($Orc->getDamage());
+// echo " Il reste " . $hero->getHealth() . " Points de vie à " . $hero->getName() . "<br>";
+
+
+
+while ($hero->getHealth() > 0 && $Orc->getHealth() > 0)  {
 echo $hero->getName() .  " Attaque " . $Orc->getName() . " de " .  $hero->getWeaponDamage() . " dégâts " . "<br>";
  echo "Il reste " .  $Orc->getHealth() - $hero->getWeaponDamage() . " Points de vie à " . $Orc->getName() . "<br>"; 
  
@@ -32,6 +44,7 @@ echo $hero->getName() .  " Attaque " . $Orc->getName() . " de " .  $hero->getWea
 echo $Orc->getName() . " Attaque " . $hero->getName() . " de " . $Orc->getDamage() . " dégâts " . "<br>";
 $hero->beAttacked($Orc->getDamage());
 echo " Il reste " . $hero->getHealth() . " Points de vie à " . $hero->getName() . "<br>";
+}
 
 ?>
 
